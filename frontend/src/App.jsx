7e5b94747ecb6d3639d423292209a107e2c39896
Oliv751 <olivierlopez751@gmail.com>
2023-01-11 20:00:18 +0100

    migrated Geoloc function into a component and add it to the app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,77 +1,14 @@
-import { useGeolocated } from "react-geolocated";
-
 import List from "./pages/List";
+import Geolocation from "./Components/Geolocation";
 
 import "./style/App.css";
 
-/* function useGeolocated() {
-  const [coords, setCoords] = useState();
-
-  useEffect(() => {
-    function success(pos) {
-      setCoords(pos.coords);
-    }
-
-    function error(err) {
-      console.error(`ERROR(${err.code}): ${err.message}`);
-    }
-
-    const options = {
-      enableHighAccuracy: false,
-      timeout: 5000,
-      maximumAge: 0,
-    };
-
-    const id = navigator.geolocation.watchPosition(success, error, options);
-
-    return () => {
-      navigator.geolocation.clearWatch(id);
-    };
-  }, []);
-  return { coords };
-} */
-
 function App() {
-  const { coords } = useGeolocated({
-    positionOptions: {
-      enableHighAccuracy: false,
-    },
-    userDecisionTimeout: 5000,
-  });
-
   return (
     <div>
       <List />;
-      {coords ? (
-        <table>
-          <tbody>
-            <tr>
-              <td>latitude</td>
-              <td>{coords.latitude}</td>
-            </tr>
-            <tr>
-              <td>longitude</td>
-              <td>{coords.longitude}</td>
-            </tr>
-            <tr>
-              <td>altitude</td>
-              <td>{coords.altitude}</td>
-            </tr>
-            <tr>
-              <td>heading</td>
-              <td>{coords.heading}</td>
-            </tr>
-            <tr>
-              <td>speed</td>
-              <td>{coords.speed}</td>
-            </tr>
-          </tbody>
-        </table>
-      ) : (
-        <div>Getting the location data&hellip; </div>
-      )}
+      <Geolocation />;
     </div>
   );
 }
-
 export default App;

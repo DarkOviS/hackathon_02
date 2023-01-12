f450e81f1bc07c1c246fef1ee596959b71fcef95
AlexisCode22 <durinalexis@gmail.com>
2023-01-12 12:02:54 +0100

    style + first fonctionnalité buttons (filter and trajet)

--- frontend/src/components/Buttons.jsx
+++ frontend/src/components/Buttons.jsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
+import "../style/Buttons.css";
 
 function Buttons() {
   const [openMenu, setOpenMenu] = useState(false);
 
   return (
     <>
-      <button
-        type="button"
-        className="buttonFilter"
-        onClick={() => setOpenMenu(!openMenu)}
-      >
-        Filter
-      </button>
-      <button type="button" className="buttonTrajet">
-        Trajet
-      </button>
+      <div className="buttonsContainer">
+        <button
+          type="button"
+          className="buttonFilter"
+          onClick={() => setOpenMenu(!openMenu)}
+        >
+          Filter
+        </button>
+        <button type="button" className="buttonTrajet">
+          Trajet
+        </button>
+      </div>
       {openMenu && <div>Menu filtre</div>}
     </>
   );

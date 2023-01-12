a87e5af705521b9c110056da9efca22f7a8b8255
Oliv751 <olivierlopez751@gmail.com>
2023-01-12 12:17:36 +0100

    create filter page

---
+++ frontend/src/pages/FiltersPage.jsx
@@ -0,0 +1,43 @@
+export default function FiltersPage() {
+  return (
+    <main>
+      <label htmlFor="price">Price:</label>
+      <p>Minimum:</p>
+      <input type="number" />
+      <p className="maxPrice">Maximum:</p>
+      <input type="number" />
+      <br />
+      <label htmlFor="disponibilité">Disponibilité:</label>
+      <input type="checkbox" />
+      <br />
+      <label htmlFor="permis">Permis:</label>
+      <p className="permisStatusB">B:</p>
+      <input type="checkbox" />
+      <p className="permisStatusB">Poids Lourd:</p>
+      <input type="checkbox" />
+      <br />
+      <label htmlFor="carburant">Carburant:</label>
+      <p className="carburantElectrique">Electrique:</p>
+      <input type="checkbox" />
+      <p className="carburantEssence">Essence:</p>
+      <input type="checkbox" />
+      <p className="carburantDiesel">Diesel:</p>
+      <input type="checkbox" />
+      <br />
+      <label htmlFor="nbrPersonne">Nombre de personnes:</label>
+      <p className="pers4">4:</p>
+      <input type="checkbox" />
+      <p className="pers5">5:</p>
+      <input type="checkbox" />
+      <p className="pers6">6:</p>
+      <input type="checkbox" />
+      <p className="pers7">7:</p>
+      <input type="checkbox" />
+      <p className="pers8">8:</p>
+      <input type="checkbox" />
+      <p className="pers9">9:</p>
+      <input type="checkbox" />
+      <br />
+    </main>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,7 +10,6 @@ export default function Home() {
       <ListContainer />
       <p>coucou</p>
       <Buttons />
-
     </main>
   );
 }

93f2626881aa09b1f75bb7f0259e6fca2b5d18ac
Rémi Lagreze <remilagr02@gmail.com>
2023-01-12 09:50:09 +0100

    Added more data and changed css of vignettes

--- frontend/src/pages/List.jsx
+++ frontend/src/pages/List.jsx
@@ -1,32 +1,7 @@
-import Header from "../Components/Header";
-import Vignette from "../Components/Vignette";
+import Header from "../components/Header";
+import Vignette from "../components/Vignette";
+import campingCarData from "../services/datalist";
 
-const campingCarData = [
-  {
-    id: 1,
-    name: "campingcar1",
-    image: "src/assets/campingcar1.png",
-    price: "100,000€",
-    available: "Disponible",
-    license: "Permis B",
-  },
-  {
-    id: 2,
-    name: "campingcar2",
-    image: "src/assets/campingcar1.png",
-    price: "45,000€",
-    available: "En maintenance",
-    license: "Permis poids lourd",
-  },
-  {
-    id: 3,
-    name: "campingcar3",
-    image: "src/assets/campingcar1.png",
-    price: "80,000€",
-    available: "Déjà loué",
-    license: "Permis B",
-  },
-];
 export default function List() {
   return (
     <div>

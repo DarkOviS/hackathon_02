e45253ba41c57d5cef1d1673c3d8636aaa2722cb
AlexisCode22 <durinalexis@gmail.com>
2023-01-12 10:43:17 +0100

    header finish, one details resolve

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
+import Header from "../components/Header";
 import List from "./List";
+import "../style/header.css";
 
 export default function Home() {
   return (
     <main>
+      <Header />
       <List />
-      <p>coucou</p>
     </main>
   );
 }

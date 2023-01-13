7da19daf3de44b08f7c46bc2e1291944cd96a582
SIMON BULLADO <s_bullado51@hotmail.com>
2023-01-13 09:33:13 +0100

    en attente

--- frontend/src/pages/CampingCarDetails.jsx
+++ frontend/src/pages/CampingCarDetails.jsx
@@ -1,18 +1,34 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import Footer from "../components/Footer";
 import campingCarData from "../services/datalist";
+import styles from "../style/CampingCarDetails.module.css";
+import Header from "../components/Header";
 
 export default function CampingCarDetails() {
   const { id } = useParams();
   const campingCar = campingCarData.find((c) => c.id === parseInt(id, 10));
 
   return (
-    <div>
-      <h1>{campingCar.name}</h1>
-      <img src={campingCar.image} alt="campingcar" />
-      <p>Price: {campingCar.price}</p>
-      <p>Availability: {campingCar.availability}</p>
-      <p>LicenseRequired: {campingCar.licenseRequired}</p>
+    <div className={styles.container}>
+      <Header />
+      <div>
+        <h1 className={styles.line}>{campingCar.name}</h1>
+        <p className={styles.line}>Prix: {campingCar.price}</p>
+        <p className={styles.line}>Statue: {campingCar.Availability}</p>
+        <p className={styles.line}>
+          Permis Requis: {campingCar.LicenseRequired}
+        </p>
+      </div>
+      <div className={styles.pic}>
+        <img
+          style={{ backgroundImage: `url(${campingCar.image})` }}
+          className={styles.img}
+          src={campingCar.image}
+          alt={campingCar.name}
+        />
+      </div>
+      <Footer />
     </div>
   );
 }
